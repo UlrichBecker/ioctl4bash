48b460808e571f6e0f0dd7d9c6a62c85aeb74af3
Ulrich Becker <u.becker@gsi.de>
2025-04-01 13:10:38 +0200

    BIN_NAME

--- buildroot/package/ioctl4bash/ioctl4bash.mk
+++ buildroot/package/ioctl4bash/ioctl4bash.mk
@@ -8,23 +8,25 @@
 ## Date:   31.03.2025                                                        ##
 ###############################################################################
 IOCTL4BASH_VERSION = master
-IOCTL4BASH_SITE = git://github.com/UlrichBecker/ioctl4bash.git
+IOCTL4BASH_SITE = https://github.com/UlrichBecker/ioctl4bash.git
 IOCTL4BASH_SITE_METHOD = git
 IOCTL4BASH_INSTALL_STAGING = YES
 
-IOCTL4BASH_LICENSE = MIT #GPL-2.0
+IOCTL4BASH_LICENSE = MIT
 IOCTL4BASH_LICENSE_FILES = COPYING
 
 IOCTL4BASH_BUILD_DIR = $(BUILD_DIR)/ioctl4bash-$(IOCTL4BASH_VERSION)
 IOCTL4BASH_INSTALL_DIR = $(TARGET_DIR)/usr/bin
 
+BIN_NAME = ioctl
+
 define IOCTL4BASH_BUILD_CMDS
-   $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+   $(MAKE) $(TARGET_CONFIGURE_OPTS) EXE_NAME=$(BIN_NAME) -C $(@D)
 endef
 
 define IOCTL4BASH_INSTALL_TARGET_CMDS
    $(INSTALL) -d $(IOCTL4BASH_INSTALL_DIR)
-   $(INSTALL) -m 0755 $(IOCTL4BASH_BUILD_DIR)/ioctl $(IOCTL4BASH_INSTALL_DIR)
+   $(INSTALL) -m 0755 $(IOCTL4BASH_BUILD_DIR)/$(BIN_NAME) $(IOCTL4BASH_INSTALL_DIR)
 endef
 
 $(eval $(generic-package))
